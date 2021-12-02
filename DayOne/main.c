54e3b17f76a1d23b2d2411e19324fa7965322d59
Ryan Myers <ryan.p.myers@gmail.com>
2021-12-02 09:24:17 -0500

    Fix tabs

--- DayOne/main.c
+++ DayOne/main.c
@@ -14,21 +14,21 @@ int main(void)
     }
 
     char buffer[MAX_LEN];
-	int previousValue = 0;
-	int valuesHigher = 0;
+    int previousValue = 0;
+    int valuesHigher = 0;
     while (fgets(buffer, MAX_LEN, fp)) {
         // Remove trailing newline
         buffer[strcspn(buffer, "\n")] = 0;
         //printf("%s\n", buffer);
-		if (atoi(buffer) > previousValue) {
-			//printf("%s is > %i \n", buffer, previousValue);
-			valuesHigher++;
-		}
-		previousValue = atoi(buffer);
+        if (atoi(buffer) > previousValue) {
+            //printf("%s is > %i \n", buffer, previousValue);
+            valuesHigher++;
+        }
+        previousValue = atoi(buffer);
     }
 
-	//Just remove one from here because the first value is going to be higher than the start of 0 by default
-	printf("Values Higher: %i\n", valuesHigher - 1);
+    //Just remove one from here because the first value is going to be higher than the start of 0 by default
+    printf("Values Higher: %i\n", valuesHigher - 1);
 
     fclose(fp);
     return 0;
